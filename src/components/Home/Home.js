9c83a1b3fba2196a79e8a82a8b6c9b3ec1c88245
azizktata <65952489+azizktata@users.noreply.github.com>
2022-07-03 20:24:03 +0100

    modified*

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,7 +8,7 @@ export default function Home() {
         <div className='personal-info'>
             <p className='hi'>Hi all. I am</p>
             <h3 className='Myname'>Aziz Ktata</h3>
-            <p className='job'> {'>'} Front-end developer</p>
+            <p className='job'> {'>'} Software developer</p>
             <p className="gitlink"> <span className='const'>const </span> <span className='github'>githubLink = </span> <a className='link' href='https://github.com/azizktata' rel='noreferrer' target="_blank" style={{textDecoration:"none"}}>"https://github.com/azizktata"</a></p>
         </div>
         <div className='layers'>
